638905f4fabd6205c6c74298e3fa679eea7f26e9
Rosie O'Marrow <rosieomarrow@me.com>
2024-12-17 10:26:06 -0500

    Initial Commit

---
+++ Tarot/TarotApp.swift
@@ -0,0 +1,32 @@
+//
+//  TarotApp.swift
+//  Tarot
+//
+//  Created by Rosie O'Marrow on 12/17/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TarotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TarotTests/TarotTests.swift
@@ -0,0 +1,17 @@
+//
+//  TarotTests.swift
+//  TarotTests
+//
+//  Created by Rosie O'Marrow on 12/17/24.
+//
+
+import Testing
+@testable import Tarot
+
+struct TarotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
